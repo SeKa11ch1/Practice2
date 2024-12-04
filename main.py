06db349854501507f350452478e08345b85ee488
SeKa11ch1 <SergeyK625@gmail.com>
2024-12-04 16:57:13 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from datetime import datetime, timedelta
+
+app = Flask(__name__)
+
+@app.route("/")
+def countdown_to_saturday():
+    today = datetime.now()
+    # Визначаємо кількість днів до суботи (5 - субота в Python)
+    days_until_saturday = (5 - today.weekday() + 7) % 7 or 7
+    return f"<h1>Днів до наступної суботи: {days_until_saturday}</h1>"
+
+if __name__ == "__main__":
+    app.run(debug=True)
